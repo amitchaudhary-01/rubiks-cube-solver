a7018330eec60bc1f42ab0d99e78fbdd13ef33a1
amitchaudhary-01 <ac984939@gmail.com>
2026-09-06 23:47:18 +0545

    importing cubeditor

--- cube-solver/src/app/scanner/page.tsx
+++ cube-solver/src/app/scanner/page.tsx
@@ -1,9 +1,11 @@
+import CubeEditor from "@/src/components/cube/CubeEditor";
 import CubeScanner from "@/src/components/scanner/CubeScanner";
 
 export default function ScannerPage() {
   return (
     <main className="min-h-screen bg-[#FBFBFA] text-zinc-900">
       <CubeScanner />
+      <CubeEditor/>
     </main>
   );
 }
